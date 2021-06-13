61df035d41bb97d7e3bd0833c19064b59de81e16
JoeyTheHarryPotter <74834315+JoeyTheHarryPotter@users.noreply.github.com>
2021-06-13 16:38:17 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,29 @@
+function setup() {
+  canvas = createCanvas(475, 475);
+  canvas.center();
+  video = createCapture(VIDEO);
+  video.hide();
+
+  classifier = ml5.imageClassifier('MobileNet', modelLoaded);
+}
+
+function draw(){
+  image(video, 0, 0, 475, 475);
+
+  classifier.classify(video, gotResult);
+}
+
+function modelLoaded(){
+  console.log("Model Loaded!");
+}
+
+function gotResult(error, results){
+  if(error){
+    console.error(error);
+  }
+  else{
+    console.log(results);
+    document.getElementById("result_object_name").innerHTML=results[0].label;
+    document.getElementById("result_object_accuracy").innerHTML=results[0].confidence.toFixed(3);
+  }
+}
